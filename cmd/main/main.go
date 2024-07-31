3a915b724007834ae1720d91632e4e0c99a8c811
Michael Chukwu <imchukwu@gmail.com>
2024-07-31 08:41:20 +0100

    updated main.go file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,25 +1,16 @@
-// cmd/main/main.go
+// 
 
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"listof/pkg/routes"
 	"listof/pkg/config"
 )
 
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	router := config.SetupApp()
-	
-	// Initialize routes
 	routes.SetupRoutes(router)
-
-	// Start server
-	log.Println("Server listening on port 8080...")
-	err := http.ListenAndServe(":8081", router)
-	if err != nil {
-		log.Fatal(err)
-	}
+	router.ServeHTTP(w, r)
 }
