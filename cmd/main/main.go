ed0f8b2c4eb1b0164a04206d08e08d01912b0ad6
Michael Chukwu <imchukwu@gmail.com>
2024-07-31 08:48:23 +0100

    working with app/index.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,16 +1,25 @@
-// 
+// cmd/main/main.go
 
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"listof/pkg/routes"
 	"listof/pkg/config"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func main() {
 	router := config.SetupApp()
+	
+	// Initialize routes
 	routes.SetupRoutes(router)
-	router.ServeHTTP(w, r)
+
+	// Start server
+	log.Println("Server listening on port 8080...")
+	err := http.ListenAndServe(":8081", router)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
